67579d83ffd8e02823372eae9fc49ee3f65f899c
Théo BIET <dev.theobiet@gmail.com>
2021-09-04 15:38:59 +0200

    🎒 Limit to 50 max exploration per user

--- app/controllers/email.js
+++ app/controllers/email.js
@@ -1,23 +1,37 @@
 const { ERROR } = require('../constants');
+const { PasswordToken, Report } = require('../models');
+const { email: emailUtils } = require('../utils');
+const crypto = require('crypto');
 
 module.exports = {
-    forgotPassword: (req, res) => {
+    forgotPassword: async (req, res) => {
         try {
             const { email } = req.body;
             const lowerEmail = email.toLowerCase();
-            const user = req.user;
+            const user = await User.findOne({ where: { email: lowerEmail } });
 
-            if (user.email !== lowerEmail) {
+            if (!user) {
                 return res.status(400).json({
-                    message: ERROR.EMAIL_NOT_MATCH,
+                    message: ERROR.EMAIL_NOT_FOUND
                 });
             }
 
-            // TODO : Send a message to the user with a link to reset password
+            const tokenPassword = crypto.randomBytes(32).toString('hex');
 
-            return res.status(200).json({
-                message: 'Email sent',
+            await PasswordToken.create({
+                user_id: user.id,
+                token: tokenPassword,
             });
+
+            const isSend = await emailUtils.forgotPassword(lowerEmail, `https://www.explorastro.com/password/forgot/update?token=${tokenPassword}`);
+
+            if (isSend) {
+                return res.status(200).json({
+                    message: 'A message has been sent to your email',
+                });
+            } else {
+                throw new Error('Error sending email');
+            }
 
         } catch (error) {
             console.log(error);
@@ -26,4 +40,39 @@ module.exports = {
             });
         }
     },
+
+    report: async (req, res) => {
+        try {
+            const { id } = req.user;
+            const { content, object } = req.body;
+            
+            if (!content || !object) {
+                return res.status(400).json({
+                    message: ERROR.REQUIRED_FIELDS
+                });
+            }
+            
+            await Report.create({
+                object,
+                content,
+                author_id: id,
+            });
+
+            const isSend = await emailUtils.report(content, object);
+
+            if (!isSend) {
+                throw new Error('Error sending email');
+            }
+
+            return res.status(200).json({
+                message: 'Report send',
+            });
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({
+                message: ERROR.INTERNAL_ERROR
+            });
+        }
+    }
+
 };
